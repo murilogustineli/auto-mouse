a8641ec66e1e6d940f790c35b21bf35a5bafc4a4
murilogustineli <murilogustineli@gmail.com>
2023-07-31 13:26:57 -0400

    updated random actions

--- run_mouse.py
+++ run_mouse.py
@@ -7,8 +7,15 @@ import random
 def move_mouse():
     # current mouse positions
     x, y = pyautogui.position()
-    rand = random.randint(1, 10)
-    pyautogui.moveTo(x + rand, y + rand)
+    rand = random.randint(1, 100)
+    if 0 < rand <= 25:
+        pyautogui.moveTo(x + 10, y + 10)
+    elif 25 < rand <= 50:
+        pyautogui.moveTo(x + 10, y - 10)
+    elif 50 < rand <= 75:
+        pyautogui.moveTo(x - 10, y + 10)
+    elif 75 < rand <= 100:
+        pyautogui.moveTo(x - 10, y - 10)
 
 while True:
     move_mouse()
