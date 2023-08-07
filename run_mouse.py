013fc9678d852551082492cd5be5333344d9c2f5
murilogustineli <murilogustineli@gmail.com>
2023-08-07 11:51:22 -0400

    updated logic and cleaned the code

--- run_mouse.py
+++ run_mouse.py
@@ -15,17 +15,9 @@ def move_mouse(wait_time=1):
         else:
             pyautogui.moveTo(x - 10, y)
         time.sleep(wait_time)  # replace 1 with the number of seconds you want to wait
-        count += 1 
+        count += 1
 
-    # rand = random.randint(1, 100)
-    # if 0 < rand <= 25:
-    #     pyautogui.moveTo(x + 10, y + 10)
-    # elif 25 < rand <= 50:
-    #     pyautogui.moveTo(x + 10, y - 10)
-    # elif 50 < rand <= 75:
-    #     pyautogui.moveTo(x - 10, y + 10)
-    # elif 75 < rand <= 100:
-    #     pyautogui.moveTo(x - 10, y - 10)
 
+# Run function
 if __name__ == "__main__":
     move_mouse()
