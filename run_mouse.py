382ca86f70d7bbf87b6ef7df4dad149f9600e1ed
murilogustineli <murilogustineli@gmail.com>
2023-08-07 11:41:38 -0400

    updated logic to move mouse horizontaly

--- run_mouse.py
+++ run_mouse.py
@@ -4,19 +4,23 @@ import random
 
 
 # Function to keep moving the mouse automatically
-def move_mouse():
+def move_mouse(wait_time=1):
     # current mouse positions
     x, y = pyautogui.position()
-    rand = random.randint(1, 100)
-    if 0 < rand <= 25:
-        pyautogui.moveTo(x + 10, y + 10)
-    elif 25 < rand <= 50:
-        pyautogui.moveTo(x + 10, y - 10)
-    elif 50 < rand <= 75:
-        pyautogui.moveTo(x - 10, y + 10)
-    elif 75 < rand <= 100:
-        pyautogui.moveTo(x - 10, y - 10)
+    while True:
+        pyautogui.moveTo(x + 10, y)
+        time.sleep(wait_time)  # replace 1 with the number of seconds you want to wait
+        pyautogui.moveTo(x - 10, y)
+        time.sleep(wait_time)
+    # rand = random.randint(1, 100)
+    # if 0 < rand <= 25:
+    #     pyautogui.moveTo(x + 10, y + 10)
+    # elif 25 < rand <= 50:
+    #     pyautogui.moveTo(x + 10, y - 10)
+    # elif 50 < rand <= 75:
+    #     pyautogui.moveTo(x - 10, y + 10)
+    # elif 75 < rand <= 100:
+    #     pyautogui.moveTo(x - 10, y - 10)
 
-while True:
+if __name__ == "__main__":
     move_mouse()
-    time.sleep(1)  # replace 1 with the number of seconds you want to wait
